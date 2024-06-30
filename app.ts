82e4c29c1ea82f5a53ad8ae31f11f0703295baa5
Paulo Silva <paulowrsilva@gmail.com>
2024-06-30 11:38:23 -0300

    concluído a implementação dos métodos deposit e withdraw

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
+peopleAccount.deposit(1000);
+peopleAccount.withdraw(500);
+console.log(peopleAccount);
+
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
+companyAccount.deposit(5000);
+companyAccount.withdraw(100);
+console.log(companyAccount);
